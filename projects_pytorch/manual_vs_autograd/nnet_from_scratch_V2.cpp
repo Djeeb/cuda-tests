17c5ae4df92079871c708ff072f11e0784c5f204
Djeeb <jean-baptiste.gaya@hec.edu>
2019-06-26 16:56:16 +0200

    new project : evaluate autograd

---
+++ projects_pytorch/manual_vs_autograd/nnet_from_scratch_V2.cpp
@@ -0,0 +1,13 @@
+#include "nnet_from_scratch_V2.hpp"
+using namespace std;
+
+int main(){
+	
+	nnet neuralnet(784,64,10);
+	
+	cout << neuralnet.parameters()[3] << endl;	
+	
+                     
+                     
+       
+}
